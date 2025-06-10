038881624850ac9b119de0f5a47fb2e835d4b414
Kevin Xiao <aquarius.c.c.a@gmail.com>
2025-06-10 16:41:18 +0800

    feat: 完成 MainWrapper 區塊

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import HeaderWrapper from './components/HeaderWrapper.vue';
+import MainWrapper from './components/MainWrapper.vue';
 </script>
 
 <template>
   <div class="app">
     <HeaderWrapper />
+    <MainWrapper />
   </div>
 </template>
 
